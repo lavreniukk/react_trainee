125220923ff85a836e76dca8e93b05d4825363dd
Andrii Lavreniuk <lavreniuk.andrey@gmail.com>
2022-11-27 14:46:22 +0200

    menu component added

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,19 +1,25 @@
 import logo from './logo.svg';
-import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent';
+import {Navbar, NavbarBrand} from 'reactstrap';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color='primary'>
-        <div className='container'>
-          <NavbarBrand className='float-start' href="/">
-            Ristorante Con Fusion
-          </NavbarBrand>
-        </div>
-      </Navbar>
-    </div>
-  );
+class App extends Component {
+  render () {
+      return (
+      <div className="App">
+        <Navbar dark color='primary'>
+          <div className='container'>
+            <NavbarBrand className='float-start' href="/">
+              Ristorante Con Fusion
+            </NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu/>
+      </div>
+    );
+  }
 }
+  
 
 export default App;
